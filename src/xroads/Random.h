65d6206cbd273f732174b18e12cee5c5f37dd2c8
Angela McEgo <eve.loikkanen@gmail.com>
2023-11-18 22:49:10 +0200

    Added "RandomPointInTetrahedron"
    Great algorithm! I like it.

--- src/xroads/Random.h
+++ src/xroads/Random.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <random>
+#include <concepts>
+#include <span>
+
 namespace Xroads
 {
     void SeedRandom(u32 n);
@@ -24,4 +28,32 @@ namespace Xroads
     //out, two normally distributed values
     std::pair<double,double> RandomNormal(double a, double b, double sigma);
     double RandomNormal(double sigma=1.0);
+
+    //from https://vcg.isti.cnr.it/activities/OLD/geometryegraphics/pointintetraedro.html
+    inline C3 RandomPointInTetrahedron(const C3& p1, const C3& p2, const C3& p3, const C3& p4)
+    {
+        double s = RandomDouble();
+        double t = RandomDouble();
+        double u = RandomDouble();
+        if(s+t>1.0)
+        {
+            s = 1.0 - s;
+            t = 1.0 - t;
+        }
+        if(t+u>1.0)
+        {
+            double tmp = u;
+            u = 1.0 - s - t;
+            t = 1.0 - tmp;
+
+        }
+        else if(s+t+u>1.0)
+        {
+            double tmp = u;
+            u = s + t + u - 1.0;
+            s = 1.0 - t - tmp;
+        }
+        double a = 1.0-s-t-u;
+        return p1*a + p2*s + p3*t + p4*u;
+    }
 }
