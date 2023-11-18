a755e79db6f7fd6d5bc4f8996eb29c28b96acb94
Angela McEgo <eve.loikkanen@gmail.com>
2023-11-18 22:50:04 +0200

    Update Xroads.h

--- src/xroads/Xroads.h
+++ src/xroads/Xroads.h
@@ -2,39 +2,15 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 #include "glad/gl.h"
 #include <GLFW/glfw3.h>
 #include <chrono>
 
-#include "XrAssert.h"
-#include "Concepts.h"
 #include "Types.h"
-#include "Color.h"
-#include "FileUtil.h"
-#include "Coords.h"
-#include "Mathutil.h"
-#include "Textures.h"
-#include "Cpputil.h"
-#include "Strutil.h"
-#include "Containers.h"
-#include "Containerutil.h"
-#include "Random.h"
-#include "IDutil.h"
-#include "Renderer.h"
-#include "Shapes.h"
-#include "Shaders.h"
-#include "Textures.h"
 
 namespace Xroads
 {
-    [[noreturn]] inline void Kill([[maybe_unused]] const std::string& s)
-    {
-#ifdef XR_VERBOSE_ERRORS
-        std::cerr << s << std::endl;
-#endif // XR_VERBOSE_ERRORS
-        std::abort();
-    }
-
     struct CloqueVal
     {
         i64 n{};
@@ -44,10 +20,52 @@ namespace Xroads
 
         auto operator<=>(const CloqueVal&) const = default;
     };
+
+    extern const CloqueVal starttime;
+
     inline CloqueVal Cloque()
     {
-        return {std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count()};
+        return {std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count()-starttime.n};
     }
 }
 
+#include "Logging.h"
+
+namespace Xroads
+{
+    void BuildStackTrace();
+
+    [[noreturn]] inline void Kill(std::string_view s)
+    {
+        Log(s);
+        BuildStackTrace();
+        std::abort();
+    }
+}
+
+#include "XrAssert.h"
+#include "Concepts.h"
+#include "Strutil.h"
+#include "Color.h"
+#include "FileUtil.h"
+#include "Coords.h"
+#include "Mathutil.h"
+#include "Textures.h"
+#include "Cpputil.h"
+#include "Containers.h"
+#include "Containerutil.h"
+#include "Random.h"
+#include "IDutil.h"
+#include "Renderer.h"
+#include "Shapes.h"
+#include "Shaders.h"
+#include "Textures.h"
+#include "Algorithms.h"
+
+namespace Xroads
+{
+    void SelfTest();
+}
+
 namespace Xr = Xroads; //comment out if the short name conflicts with something
+
